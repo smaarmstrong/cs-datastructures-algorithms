05f0b8068f9014eb2d2df70d0a78e5c370dd9fe1
seanm <seanm@CAMINANDO>
2024-01-29 11:59:56 -0800

    Add project files.

---
+++ cs-datastructures-algorithms/ADT/Pokemon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_datastructures_algorithms.ADT
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public int Exp { get; set; }
+
+        public bool isPokemon()
+        {
+            return true;
+        }
+    }
+}
---
+++ cs-datastructures-algorithms/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using cs_datastructures_algorithms.ADT;
+using System.Collections;
+
+Stack ts = new Stack();
+
+Pokemon squirtle = new Pokemon();
